f4fc42bf3910dc63a4a4547cc06dec6489a993ed
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-07-12 00:17:59 +0300

    Delete SOLID Principles/OCP/NotificationService.java

--- SOLID Principles/OCP/NotificationService.java
+++
@@ -1,7 +0,0 @@
-// Step 3: NotificationService depends on abstraction
-
-public class NotificationService {
-    public void sendNotification(Notifier notifier, String message) {
-        notifier.send(message);
-    }
-}
