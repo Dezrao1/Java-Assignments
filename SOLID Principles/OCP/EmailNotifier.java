ee35322ea8a10bbfc760f65a2aa98f3a8f19ea02
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-07-12 00:16:56 +0300

    Delete SOLID Principles/OCP/EmailNotifier.java

--- SOLID Principles/OCP/EmailNotifier.java
+++
@@ -1,8 +0,0 @@
-// Step 2: Implement different notifiers
-
-public class EmailNotifier implements Notifier {
-    @Override
-    public void send(String message) {
-        System.out.println("Sending Email: " + message);
-    }
-}
