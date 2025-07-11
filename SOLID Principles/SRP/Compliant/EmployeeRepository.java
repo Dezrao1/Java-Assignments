b923d48f445b179b1cb0997140c982512cf1a782
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-07-12 00:08:41 +0300

    Create EmployeeRepository.java

---
+++ SOLID Principles/SRP/Compliant/EmployeeRepository.java
@@ -0,0 +1,6 @@
+class EmployeeRepository {
+    public void saveToDatabase(Employee employee) {
+        // Database saving logic
+        System.out.println("Saving employee to database...");
+    }
+}
