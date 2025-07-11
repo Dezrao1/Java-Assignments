4cec517dca3d7be458161e2b9e61415d204a1fe4
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-07-11 23:01:11 +0300

    Create Greeter.java

---
+++ Challenge 1/Greeter.java
@@ -0,0 +1,11 @@
+package Challenge 1;
+
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+    public static void main(String[] args) {
+
+        new Greeter().greet();
+    }
+}
