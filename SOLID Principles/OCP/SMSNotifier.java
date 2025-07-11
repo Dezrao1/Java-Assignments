87544e8352c3c4c81908a99963de32c00470f854
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-07-12 00:18:27 +0300

    Delete SOLID Principles/OCP/SMSNotifier.java

--- SOLID Principles/OCP/SMSNotifier.java
+++
@@ -1,8 +0,0 @@
-// Step 2: Implement different notifiers
-
-public class SMSNotifier implements Notifier {
-    @Override
-    public void send(String message) {
-        System.out.println("Sending SMS: " + message);
-    }
-}
